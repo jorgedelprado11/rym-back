f7d1f02bff8ed5a5db0b922e10d4f152838799ca
jorgedelprado11 <jorgedelprado11@gmail.com>
2023-08-22 16:10:32 -0300

    db subida

--- src/DB_connection.js
+++ src/DB_connection.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
-const { Sequelize } = require("sequelize");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY } = process.env;
+const { Sequelize } = require("sequelize");
 const FavoriteModel = require("./models/Favorite");
 const UserModel = require("./models/User");
 
